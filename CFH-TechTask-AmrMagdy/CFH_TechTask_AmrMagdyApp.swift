50fca77822e185d5b09e74749475b11fe47d70dd
Amr Magdy <amagdy195@yahoo.com>
2024-03-19 01:12:52 +0200

    Initial Commit

---
+++ CFH-TechTask-AmrMagdy/CFH_TechTask_AmrMagdyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CFH_TechTask_AmrMagdyApp.swift
+//  CFH-TechTask-AmrMagdy
+//
+//  Created by Amr Magdy on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CFH_TechTask_AmrMagdyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
